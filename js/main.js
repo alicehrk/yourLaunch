7873f2813e37c8868454f4de3449a043ac66315b
alicehrk <alicehrk23@gmail.com>
2021-11-22 19:21:24 +0900

    순차적으로 메인 화면 요소 보이기

--- js/main.js
+++ js/main.js
@@ -1,35 +1,9 @@
-const searchEl =document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input')
-
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
-const badgeEl =document.querySelector('header .badges');
-const toTopEl =document.querySelector('#to-top');
-
-
-toTopEl.addEventListener('click', function () {
-  gsap.to(window, .7, {
-    scrollTo: 0
-  });
-})
-
-
 const fadeEls = document.querySelectorAll('.visual .fade-in');
+console.log(fadeEls);
 fadeEls.forEach(function (fadeEl, index) {
+  // gsap.to(요소, 지속시간, 옵션);
   gsap.to(fadeEl, 1, {
-    delay : (index + 1) * .7,
+    delay : (index + 1) * .5,
     opacity : 1
   });
 });
