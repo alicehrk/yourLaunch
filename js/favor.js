2c5c8df99948a30b92415ec5b10329ca02b53cc3
alicehrk <alicehrk23@gmail.com>
2021-11-24 17:02:15 +0900

    favor 1p 디자인 완성

---
+++ js/favor.js
@@ -0,0 +1,7 @@
+// local host 테스트용 코드
+
+// const first = document.querySelector('.first-container');
+
+// first.addEventListener('click', function() {
+//   localStorage.setItem('dd', 'ss');
+// });
