7b3ee5e089a3c7404378b0d8d890f4a773edc00b
alicehrk <alicehrk23@gmail.com>
2021-12-09 00:56:15 +0900

    최종 -php 코드 수정

--- local.php
+++ local.php
@@ -3,20 +3,40 @@
     $names = $_COOKIE['name'];
 
     $conn = mysqli_connect("localhost", "root", "cscscs");
-    $db_status = mysqli_select_db($conn, 'termproject');
 
-    if (!$db_status) {
-        echo "DB_ERROR";
-        exit;
+    $restaurantInformation = mysqli_select_db($conn, 'restinfo');
+    if(!$restaurantInformation) {
+        $create_db_query = "CREATE DATABASE restinfo";
+        if(mysqli_query($conn, $create_db_query)) {
+        } else {
+            return;
+        }
     }
 
-    $insert_query = "INSERT INTO rest_name (restName, cnt) VALUES ('$names', 1)
-ON DUPLICATE KEY UPDATE cnt = cnt + 1";
+    $restaurantInformation = mysqli_select_db($conn, 'restinfo');
 
-if ($conn->query($insert_query) === TRUE) {
-echo "New record created successfully";
-} else {
-echo "Error: " . $insert_query . "<br>" . $conn->error;
-}
-$conn->close();
+    $check = mysqli_query($conn, "SELECT 1 FROM 'restinfo' LIMIT 1");
+
+    if ($check == false) {
+        $create_table_query = "CREATE TABLE IF NOT EXISTS rest_name (
+            rest_name varchar(255) NOT NULL,
+            cnt int(11),
+            PRIMARY KEY (rest_name)
+            );";
+        $result = $conn->query($create_table_query);
+
+        if ($result == false) {
+            echo "Error: " . $create_table_query . "<br>" . $connect->error;
+        }
+    }
+
+    $insert_query = "INSERT INTO rest_name (rest_name, cnt) VALUES ('$names', 1)
+    ON DUPLICATE KEY UPDATE cnt = cnt + 1";
+    if ($conn->query($insert_query) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $insert_query . "<br>" . $conn->error;
+    }
+    $conn->close();
+
 ?>
